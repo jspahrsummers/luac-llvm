0e45f146c7f7f0b1bc8e61ee0c9230c2c851125e
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-04-17 19:45:36 -0700

    Added comments throughout the Expr module

--- expr.hs
+++ expr.hs
@@ -4,25 +4,43 @@ import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
+-- Represents a binary operator in an AST
 data BinaryOperator = AddOperator | SubOperator
     deriving Show
 
+-- Represents any expression in an AST
 data Expression =
+    -- Unary "not" expression
     NotExpression Expression |
+    
+    -- A literal number value
     NumberLiteral Double |
+
+    -- A binary expression
     BinaryExpression BinaryOperator Expression Expression
+
     deriving Show
 
+-- Defines all Lua tokens
 languageDef = emptyDef {
     Token.reservedOpNames = ["+", "-", "not"]
 }
 
 lexer = Token.makeTokenParser languageDef
+
+-- Parses reserved operators in Lua
 reservedOp = Token.reservedOp lexer
+
+-- Parses parentheses
 parens = Token.parens lexer
+
+-- Parses any integer or floating-point literal
 number = Token.naturalOrFloat lexer
+
+-- Parses any spaces
 whiteSpace = Token.whiteSpace lexer
 
+-- Defines all Lua operators, their precedence, and their associativity
 operators = [
     [Prefix (reservedOp "not" >> return (NotExpression))],
     [Infix (reservedOp "+" >> return (BinaryExpression AddOperator)) AssocLeft],
@@ -36,8 +54,10 @@ term =
     parens expression <|>
     liftM (NumberLiteral . doubleFromEither) number
 
+-- Parses expressions
 expression :: Parser Expression
 expression = buildExpressionParser operators term
 
-compile :: String -> Either ParseError Expression
-compile str = parse expression "" str
+-- Parses a Lua string into an expression AST
+compileExpression :: String -> Either ParseError Expression
+compileExpression str = parse expression "" str
