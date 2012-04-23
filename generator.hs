c83a48cf3be16acb5dff959a366018656543526d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-04-22 17:53:23 -0700

    Added code generation for NotExpressions

--- generator.hs
+++ generator.hs
@@ -1,6 +1,6 @@
 module Generator where
 
-import Parser
+import qualified Parser
 import System.IO
 
 -- Writes an LLVM assembly statement
@@ -14,6 +14,14 @@ label fd name = do
     hPutStrLn fd (name ++ ":")
 
 -- Writes an LLVM number literal
-expression :: Handle -> Expression -> IO ()
-expression fd (NumberLiteral num) = do
+expression :: Handle -> Parser.Expression -> IO ()
+
+expression fd (Parser.NumberLiteral num) = do
     statement fd ("call %lua_pushnumber_fp @lua_pushnumber (%lua_State* %state, %lua_Number " ++ (show num) ++ ")")
+
+expression fd (Parser.NotExpression expr) = do
+    expression fd expr
+    statement fd ("%value = call %lua_toboolean_fp @lua_toboolean (%lua_State* %state, i32 -1)")
+    statement fd ("call %lua_pop_fp @lua_pop (%lua_State* %state, i32 1)")
+    statement fd ("%negatedValue = xor i32 %value, 1")
+    statement fd ("call %lua_pushboolean_fp @lua_pushboolean (%lua_State* %state, i32 %negatedValue)")
