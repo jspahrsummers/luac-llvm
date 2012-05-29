4cf3529b266144d6cd7e363b9541d7d65d192557
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-05-28 17:53:44 -0700

    Exit with a failure error code when parsing fails

--- main.hs
+++ main.hs
@@ -3,11 +3,16 @@ module Main where
 import Generator
 import IO
 import Parser
+import System.Exit
 import System.IO
 import Text.ParserCombinators.Parsec
+
+printErrorAndExit e = do
+    putStrLn (show e)
+    exitFailure
 
 main :: IO ()
 main = do
     outFD <- openFile "output.ll" WriteMode
     contents <- getFileContents "input.lua"
-    either (putStrLn . show) (putTopLevelExpression outFD) (parse Parser.exp "" contents)
+    either printErrorAndExit (putTopLevelExpression outFD) (parse Parser.exp "" contents)
