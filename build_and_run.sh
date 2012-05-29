75fb7ba0bc7cbcdf09a639025236832ea27584ae
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-05-28 17:48:46 -0700

    Clarify the steps executed in the build_and_run script, and exit early after any errors occur

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-echo "*** Compiling compiler"
+echo "*** Compiling compiler and running on sample input file"
 ghc -e 'main' main.hs
 
-echo "*** Running compiler on sample input file"
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+echo "*** Compiling generated LLVM assembly"
 llc -O0 -o output.S output.ll && clang -fcatch-undefined-behavior -ftrapv -O0 -llua -o output output.S
+
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
 
 echo "*** Running sample program"
 ./output
